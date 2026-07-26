2b8218164514aeedcce34f58a22d7168e6d6a997
dimmimik <dimmimik@ukr.net>
2026-07-26 20:58:52 +0300

    Create expenses.py

---
+++ data/expenses.py
@@ -0,0 +1,43 @@
+EXPENSES = [
+
+    {
+
+        "category":"Food",
+
+        "amount":24.90,
+
+        "month":"July"
+
+    },
+
+    {
+
+        "category":"Transport",
+
+        "amount":12.00,
+
+        "month":"July"
+
+    },
+
+    {
+
+        "category":"Food",
+
+        "amount":15.60,
+
+        "month":"August"
+
+    },
+
+    {
+
+        "category":"Entertainment",
+
+        "amount":28.40,
+
+        "month":"August"
+
+    }
+
+]
